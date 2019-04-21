fdca4b6be09c00dce9fd72858c44c1df510cdf7c
hatamoto <kanjihtmt@gmail.com>
2019-04-21 21:06:34 +0900

    管理者管理機能を実装

--- app/controllers/admin/administrators_controller.rb
+++ app/controllers/admin/administrators_controller.rb
@@ -1,23 +1,41 @@
 class Admin::AdministratorsController < Admin::BaseController
   before_action :set_administrator, only: %i(edit update destroy)
-  before_action :check_super_admin, only: %i(destroy)
+  before_action :check_admin, only: %i(destroy)
 
   def index
+    @administrators = Administrator.page(params[:page])
   end
 
   def new
+    @administrator = Administrator.new
   end
 
   def edit
   end
 
   def create
+    @administrator = Administrator.new(administrator_params)
+    if @administrator.save
+      redirect_to admin_administrators_path,
+        notice: t(:create, scope: 'flash_message', model: Administrator.model_name.human)
+    else
+      render :new
+    end
   end
 
   def update
+    if @administrator.update(administrator_params)
+      redirect_to admin_administrators_path,
+                  notice: t(:update, scope: 'flash_message', model: Administrator.model_name.human)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @administrator.destroy
+    redirect_to admin_administrators_path,
+      notice: t(:destroy, scope: 'flash_message', model: Administrator.model_name.human)
   end
 
   private
@@ -27,5 +45,12 @@ class Admin::AdministratorsController < Admin::BaseController
     end
 
     def check_admin
+      if @administrator.admin? && Administrator.admin.count == 1
+        redirect_to admin_administrators_path, notice: t(:can_not_only_admin_user_error)
+      end
+    end
+
+    def administrator_params
+      params.require(:administrator).permit(:username, :password, :role)
     end
 end
