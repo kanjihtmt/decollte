cffd814f490da950114053ec7ce0158e52a79545
hatamoto <kanjihtmt@gmail.com>
2019-04-23 21:16:44 +0900

    トップページのブランドコントローラーを修正

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,13 +1,5 @@
 class BrandsController < ApplicationController
-  after_action :brands?
-
   def index
     @brands = Brand.page(params[:page])
   end
-
-  private
-
-    def brands?
-      raise ActiveRecord::RecordNotFound if @brands.blank?
-    end
 end
